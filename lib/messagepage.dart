b3a30178609f14c84e4c4bfc798d3311a1ca90b1
Mark Allen Talandron <markallen.talandron.20@usjr.edu.ph>
2022-11-30 17:43:40 +0800

    recoded message and implemented it properly now

--- lib/messagepage.dart
+++ lib/messagepage.dart
@@ -10,80 +10,95 @@ class MessagePage extends StatelessWidget {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     var width = size.width;
+
     return SafeArea(
         child: Scaffold(
-            body: Column(
-                children: [
-          SizedBox(
-            width: width,
-            height: 10,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: const Icon(Icons.arrow_back_sharp))
-              ],
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(Icons.arrow_back)),
+      ),
+      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        const SizedBox(
+          height: 10,
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          width: width,
+          decoration: const BoxDecoration(
+              border: Border(
+            bottom: BorderSide(color: Colors.black),
+          )),
+          child: Text(
+            message.getSubject,
+            style: const TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
             ),
           ),
-          SizedBox(
-            width: width,
-            height: 30,
-            child: Text(
-              message.getSubject,
-              style: const TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          decoration: const BoxDecoration(
+              border: Border(
+            bottom: BorderSide(color: Colors.black),
+          )),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: message.getColor,
+                shape: BoxShape.circle,
+              ),
+              child: Text(
+                message.getMessage[0],
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            width: width,
-            height: 30,
-            child: Row(
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: message.getColor,
-                    shape: BoxShape.circle,
-                  ),
+            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Text(
+                message.getName,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
                 ),
-                const SizedBox(
-                  width: 10,
+              ),
+              Text(
+                message.getSender,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
                 ),
-                Column(
-                  children: [
-                    Text(
-                      message.getName,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                      ),
-                    ),
-                    Text(
-                      message.getSender,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 15,
-                      ),
-                    )
-                  ],
-                ),
-                Text("${months[message.getMonth]} ${message.getDay}",),
-              ],
+              )
+            ]),
+            Text(
+              "${months[message.getMonth]} ${message.getDay}",
+            ),
+          ]),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Expanded(
+          child: Text(
+            message.getMessage,
+            style: const TextStyle(
+              fontSize: 30,
             ),
           ),
-          Expanded(
-            child: Text(
-              message.getMessage,
-              style: const TextStyle(
-                fontSize: 40,
-              ),
-            )
-          )
-        ])));
+        ),
+      ]),
+    ));
   }
 }
