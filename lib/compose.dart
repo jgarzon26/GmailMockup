bedfc0e1ca6f2e7daba03ede00c4a1a8ecafc338
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2022-11-30 13:05:11 +0800

    InitPage

---
+++ lib/compose.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'message.dart';
+
+class ComposePage extends StatefulWidget{
+
+  @override
+  State<ComposePage> createState() => _ComposePageState();
+}
+
+class _ComposePageState extends State<ComposePage> {
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+
+    );
+  }
+}
