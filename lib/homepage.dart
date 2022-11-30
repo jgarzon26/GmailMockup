30a41d715df25a9dda22ba88e9039e2846af6cbf
Mark Allen Talandron <markallen.talandron.20@usjr.edu.ph>
2022-11-30 10:59:20 +0800

    added more logic and changed styling

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 import 'message.dart';
 
+/*Conversion for month */
+const months = {
+  1: "January",
+  2: "February",
+  3: "March",
+  4: "April",
+  5: "May",
+  6: "June",
+  7: "July",
+  8: "August",
+  9: "September",
+  10: "October",
+  11: "November",
+  12: "December",
+};
+
+enum CurrentBox { inbox, sent, archive }
+
+var colors = [
+  Colors.red,
+  Colors.blue,
+  Colors.green,
+  Colors.amber,
+  Colors.purple,
+  Colors.pink
+];
+
 class HomePage extends StatefulWidget {
   HomePage({super.key});
   final List<Message> inbox = [];
-  final List<Message> send = [];
+  final List<Message> sent = [];
   final List<Message> archive = [];
 
   @override
@@ -18,12 +45,15 @@ class _HomePageState extends State<HomePage> {
       message: "Hello",
       sender: "helloworld@gmail.com",
       name: "World",
+      color: colors[1],
       dateReceived: DateTime.now());
   Message two = Message(
       message: "Hello, universe!",
       sender: "hellouniv@gmail.com",
       name: "Universe",
+      color: colors[4],
       dateReceived: DateTime.now());
+  var currBox = CurrentBox.inbox.index;
 
   @override
   Widget build(BuildContext context) {
@@ -55,10 +85,10 @@ class _HomePageState extends State<HomePage> {
                           color: Colors.red,
                           shape: BoxShape.circle,
                         ),
-                        child: Text(
+                        child: const Text(
                           'A',
                           textAlign: TextAlign.center,
-                          style: const TextStyle(
+                          style: TextStyle(
                             color: Colors.white,
                             fontWeight: FontWeight.bold,
                             fontSize: 30,
@@ -69,11 +99,7 @@ class _HomePageState extends State<HomePage> {
                   )),
             ),
           ),
-          widget.inbox.isEmpty
-              ? const Center(
-                  child: Text("Inbox is empty"),
-                )
-              : displayInbox(context),
+          runBoxes(),
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -91,13 +117,36 @@ class _HomePageState extends State<HomePage> {
     ));
   }
 
+  Widget runBoxes() {
+    Widget box = const Text("Empty");
+    switch (currBox) {
+      case 0:
+        box = displayInbox(context);
+        break;
+      case 1:
+        box = displaySent(context);
+        break;
+      case 2:
+        box = displayArchive(context);
+    }
+
+    return box;
+  }
+
 /*Display Inbox */
   Widget displayInbox(BuildContext context) {
+    if (widget.inbox.isEmpty) {
+      return const Expanded(
+        child: Center(
+          child: Text("Inbox is empty"),
+        ),
+      );
+    }
     return Expanded(
       child: ListView(
         children: widget.inbox.map((Message mess) {
           return Dismissible(
-              key: Key(widget.inbox.indexOf(mess).toString()),
+              key: UniqueKey(),
               background: Container(
                   color: Colors.green,
                   child: Padding(
@@ -142,8 +191,8 @@ class _HomePageState extends State<HomePage> {
                   child: Row(children: <Widget>[
                     Container(
                       padding: const EdgeInsets.all(10),
-                      decoration: const BoxDecoration(
-                        color: Colors.red,
+                      decoration: BoxDecoration(
+                        color: mess.getColor,
                         shape: BoxShape.circle,
                       ),
                       child: Text(
@@ -160,26 +209,162 @@ class _HomePageState extends State<HomePage> {
                     SizedBox(
                       width: 350,
                       child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                mess.getName,
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 15,
-                                ),
-                              ),
-                              Text(
-                                "${mess.getMonth}-${mess.getDay}",
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 15,
-                                ),
-                              )
-                            ],
+                          Text(
+                            "${months[mess.getMonth]} ${mess.getDay}",
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                          Text(
+                            mess.getName,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                          Text(
+                            mess.getMessage,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w300,
+                              fontSize: 10,
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ])));
+        }).toList(),
+      ),
+    );
+  }
+
+/*Display Sent box */
+  Widget displaySent(BuildContext context) {
+    if (widget.sent.isEmpty) {
+      return const Expanded(
+        child: Center(
+          child: Text("Inbox is empty"),
+        ),
+      );
+    }
+    return Expanded(
+      child: ListView(
+        children: widget.sent.map((Message mess) {
+          return Dismissible(
+              key: UniqueKey(),
+              child: InkWell(
+                  onTap: () {},
+                  child: Row(children: <Widget>[
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: mess.getColor,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Text(
+                        mess.getName[0],
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 50),
+                    SizedBox(
+                      width: 350,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "${months[mess.getMonth]} ${mess.getDay}",
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                          Text(
+                            mess.getName,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                          Text(
+                            mess.getMessage,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w300,
+                              fontSize: 10,
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ])));
+        }).toList(),
+      ),
+    );
+  }
+
+/*Display Archive box */
+  Widget displayArchive(BuildContext context) {
+    if (widget.archive.isEmpty) {
+      return const Expanded(
+        child: Center(
+          child: Text("Inbox is empty"),
+        ),
+      );
+    }
+    return Expanded(
+      child: ListView(
+        children: widget.archive.map((Message mess) {
+          return Dismissible(
+              key: UniqueKey(),
+              child: InkWell(
+                  onTap: () {},
+                  child: Row(children: <Widget>[
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: mess.getColor,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Text(
+                        mess.getName[0],
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 50),
+                    SizedBox(
+                      width: 350,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "${months[mess.getMonth]} ${mess.getDay}",
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
+                          ),
+                          Text(
+                            mess.getName,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15,
+                            ),
                           ),
                           Text(
                             mess.getMessage,
@@ -218,7 +403,12 @@ class _HomePageState extends State<HomePage> {
                 fontSize: 25,
               ),
             ),
-            onTap: () {}),
+            onTap: () {
+              setState(() {
+                currBox = CurrentBox.inbox.index;
+              });
+              Navigator.pop(context);
+            }),
         ListTile(
           leading: const Icon(Icons.send),
           title: const Text(
@@ -228,7 +418,12 @@ class _HomePageState extends State<HomePage> {
               fontSize: 25,
             ),
           ),
-          onTap: () {},
+          onTap: () {
+            setState(() {
+              currBox = CurrentBox.sent.index;
+            });
+            Navigator.pop(context);
+          },
         ),
         ListTile(
           leading: const Icon(Icons.archive),
@@ -239,7 +434,12 @@ class _HomePageState extends State<HomePage> {
               fontSize: 25,
             ),
           ),
-          onTap: () {},
+          onTap: () {
+            setState(() {
+              currBox = CurrentBox.archive.index;
+            });
+            Navigator.pop(context);
+          },
         ),
       ],
     ));
--- lib/message.dart
+++ lib/message.dart
@@ -2,13 +2,21 @@
 For outputting message 
 */
 
-class Message
-{
-  Message({required this.message, required this.sender, required this.name, this.subject = "None", required this.dateReceived});
+import 'dart:ui';
+
+class Message {
+  Message(
+      {required this.message,
+      required this.sender,
+      required this.name,
+      this.subject = "None",
+      required this.color,
+      required this.dateReceived});
   String message;
   String sender;
   String name;
   String subject;
+  Color color;
   DateTime dateReceived;
 
 /* Getters */
@@ -16,9 +24,9 @@ class Message
   String get getSender => sender;
   String get getName => name;
   String get getSubject => subject;
+  Color get getColor => color;
   DateTime get getDate => dateReceived;
   int get getDay => dateReceived.day;
   int get getMonth => dateReceived.month;
   int get getYear => dateReceived.year;
-
-} 
+}
