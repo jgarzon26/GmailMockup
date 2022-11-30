ab4663ade6ebe6a89441eabba2cdea930447b1f5
Janus Sean Garzon <janus.garzon@usjr.edu.ph>
2022-11-30 23:54:35 +0800

    overlays for attach and options

--- lib/compose.dart
+++ lib/compose.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'message.dart';
+import 'overlays.dart';
 
 class ComposePage extends StatefulWidget{
 
@@ -8,43 +9,69 @@ class ComposePage extends StatefulWidget{
 }
 
 class _ComposePageState extends State<ComposePage> {
-  
+
+  var _isAttachmentOpen = false;
+  var _isOptionsOpen = false;
+
   @override
   Widget build(BuildContext context){
+
+    final overlays = Overlays(context);
+
     return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(
-              Icons.arrow_back,
+      child: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () {
+          if(_isAttachmentOpen){
+            overlays.showAttachment(false);
+            _isAttachmentOpen = false;
+          }
+
+          if(_isOptionsOpen){
+            overlays.showOptions(false);
+            _isOptionsOpen = false;
+          }
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            leading: IconButton(
+              icon: Icon(
+                Icons.arrow_back,
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
             ),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-          title: Text(
-            "Compose"
-          ),
-          actions: [
-            IconButton(
+            title: Text(
+              "Compose"
+            ),
+            actions: [
+              IconButton(
+                  onPressed: () {
+                    overlays.showAttachment(true);
+                    _isAttachmentOpen = true;
+                  },
+                  icon: Icon(
+                    Icons.attachment,
+                  ),
+              ),
+              IconButton(
                 onPressed: () {},
                 icon: Icon(
-                  Icons.attachment,
+                  Icons.send,
                 ),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.send,
               ),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.more_vert,
+              IconButton(
+                onPressed: () {
+                  overlays.showOptions(true);
+                  _isOptionsOpen = true;
+                },
+                icon: Icon(
+                  Icons.more_vert,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
